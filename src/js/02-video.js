d0af8f706583fb5a241ad46b57629c9533fd1b3a
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-09 11:56:05 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Player from '@vimeo/player';  
+import throttle from 'lodash.throttle';
 
+
+const LOCALSTORAGE ="videoplayer-current-time"
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+function timeTrack() {
+  player.on('timeupdate', function (time) {
+    localStorage.setItem(LOCALSTORAGE, JSON.stringify(time.seconds));
+    
+  });
+  
+};
+
+timeTrack();
+
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
